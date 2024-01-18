db4534c286339f02b739b2c51721b4d5133a5758
Hanan Elmogey <nana.elmogey@gmail.com>
2024-01-18 16:49:03 +0200

    monty

--- mul.c
+++ mul.c
@@ -8,29 +8,29 @@
  */
 void mul_nodes(stack_t **stack_head, unsigned int line_number)
 {
-    stack_t *current_node;
-    int stack_length = 0, result;
+stack_t *current_node;
+int stack_length = 0, result;
 
-    current_node = *stack_head;
+current_node = *stack_head;
 
-    while (current_node)
-    {
-        current_node = current_node->next;
-        stack_length++;
-    }
+while (current_node)
+{
+current_node = current_node->next;
+stack_length++;
+}
 
-    if (stack_length < 2)
-    {
-        fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
-        fclose(carry.file);
-        free(carry.content);
-        flush(*stack_head);
-        exit(EXIT_FAILURE);
-    }
+if (stack_length < 2)
+{
+fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+fclose(carry.file);
+free(carry.content);
+flush(*stack_head);
+exit(EXIT_FAILURE);
+}
 
-    current_node = *stack_head;
-    result = current_node->next->n * current_node->n;
-    current_node->next->n = result;
-    *stack_head = current_node->next;
-    free(current_node);
+current_node = *stack_head;
+result = current_node->next->n * current_node->n;
+current_node->next->n = result;
+*stack_head = current_node->next;
+free(current_node);
 }
